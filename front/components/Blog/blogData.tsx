e99adad94948e9dba2fa00d99b51d81da8c6b13f
Raghav chawla <raghchawla.131@gmail.com>
2023-09-27 00:14:17 +0530

    blog

--- front/components/Blog/blogData.tsx
+++ front/components/Blog/blogData.tsx
@@ -3,24 +3,24 @@ import { Blog } from "@/types/blog";
 const blogData: Blog[] = [
   {
     id: 1,
-    title: "Best UI components for modern websites",
+    title: "REDUCE, REUSE, RECYCLE and REPAIR",
     paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "/images/blog/blog-01.jpg",
+      "Rapid advancement of technology has given rise to a significant environmental challenge: electronic waste.",
+    image: "/images/blog/reduce-reuse-recycle.jpg",
     author: {
       name: "Samuyl Joshi",
       image: "/images/blog/author-01.png",
       designation: "Graphic Designer",
     },
     tags: ["creative"],
-    publishDate: "2025",
+    publishDate: "2021",
   },
   {
     id: 2,
-    title: "9 simple ways to improve your design skills",
+    title: "The problem of E-waste in India",
     paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "/images/blog/blog-02.jpg",
+      "India generates 3.2 million metric tons of e-waste per year, and this number is only expected to grow.",
+    image: "/images/blog/e-waste-india.jpg",
     author: {
       name: "Musharof Chy",
       image: "/images/blog/author-02.png",
@@ -31,10 +31,10 @@ const blogData: Blog[] = [
   },
   {
     id: 3,
-    title: "Tips to quickly improve your coding speed.",
+    title: "Educating the Next Generation",
     paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "/images/blog/blog-03.jpg",
+      "How can school districts incorporate educational programs into their schools? Several options are available.",
+    image: "/images/blog/e-waste-edu.png",
     author: {
       name: "Lethium Deo",
       image: "/images/blog/author-03.png",
